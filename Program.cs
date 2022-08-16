dcbc082fc68b43ea0a0715211d1455fdd416b4c3
Evgeny Pogudo <sensesbit@gmail.com>
2022-08-16 22:56:18 +0300

    program

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿string[] array = new string[4] {"hello", "2", "world", ":-)"};
+string[] array2 = new string[4] {"1234", "1567", "-2", "computer sciense"};
+
+
+int HowElem(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+            count++;
+    }
+    return count;
+}
+
+string[] NewArray(string[] array)
+{
+  //  int j = 0;
+    string[] newarray = new string[HowElem(array)];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            newarray[j] = array[i];
+            j++;
+        }
+    }
+    return newarray;
+}
+
+Console.WriteLine($"[{String.Join(", ",NewArray(array))}]");
+Console.WriteLine($"[{String.Join(", ",NewArray(array2))}]");
